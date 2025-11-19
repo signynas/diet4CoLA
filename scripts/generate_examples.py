1056d4e26edc644fdfb6d8d6fc0ae9291058db20
PolyRocketMatt <rocketpencil.studios@gmail.com>
2025-11-19 18:44:27 +0100

    Add initial model notebook

--- scripts/generate_examples.py
+++ scripts/generate_examples.py
@@ -16,14 +16,6 @@ from diet4cola.velocity import compute_velocity_fields, prior_exp
 from diet4cola.sdf import sdf_box, compute_rounded_sdfs
 from diet4cola.utils import save_array, load_array
 
-# Simulation Parameters
-T_MIN = 0.0
-T_MAX = 10.0  # Total simulation time (in seconds)
-TIME_STEP = 0.1
-TIMEPOINTS = np.arange(T_MIN, T_MAX + TIME_STEP, TIME_STEP)
-ITERATIONS = len(TIMEPOINTS)
-NORMALIZED_TIMEPOINTS = TIMEPOINTS / T_MAX
-
 # Grid parameters
 WIDTH = 512
 HEIGHT = 512
@@ -34,9 +26,17 @@ def initialize_velocity_exponential(alpha: float, t: float, k: float) -> float:
 def initialize_velocity_hyperbolic(alpha: float, t: float, k: float) -> float:
     return (alpha ** 2) * (1.0 / (k * t + alpha))
 
-def gen_advected_cortex_worker(idx: int, out: str, max_offset: int, extent_major: int,
+def gen_advected_cortex_worker(idx: int, out: str, step: float, max_offset: int, extent_major: int,
                                extent_minor: int) -> dict:
-    # 1. Generate Cortex (Initial State)
+    # 1. Simulation Parameters
+    t_min = 0.0
+    t_max = 10.0  # Total simulation time (in seconds)
+    time_step = step
+    timepoints = np.arange(t_min, t_max + time_step, time_step)
+    iterations = len(timepoints)
+    normalized_timepoints = timepoints / t_max
+
+    # 2. Generate Cortex (Initial State)
     spec = CortexSpec()
     seed = np.random.randint(0, 1e8)
     np.random.seed(seed)
@@ -50,11 +50,11 @@ def gen_advected_cortex_worker(idx: int, out: str, max_offset: int, extent_major
     off_x = WIDTH // 2 + (np.random.randint(32) - 16)
     off_y = HEIGHT // 2 + (np.random.randint(32) - 16)
     
-    # 2. Create CoLA Cut
+    # 3. Create CoLA Cut
     cola_cut = CoLACut((off_x, off_y), 32, 150, seed)
 
-    # 3. Compute Time-Dependent SDF/Velocity Parameters
-    # 3a. Sample initial parameters
+    # 4. Compute Time-Dependent SDF/Velocity Parameters
+    # 4a. Sample initial parameters
     sdf_radius = np.random.rand() * 5 # Max SDF radius parameter of 5
     alpha = np.random.rand() * 10 # Max initial velocity of 10
     falloff_distance = np.random.randint(0, 150) # Max falloff distance of 150
@@ -65,62 +65,62 @@ def gen_advected_cortex_worker(idx: int, out: str, max_offset: int, extent_major
     else:
         k = np.random.rand() * 10
 
-    # 3b. Create velocity profiles for the edge of the SDF
-    initial_velocities = np.array([initialize_velocity_exponential(alpha, t, k) if use_exponential else initialize_velocity_hyperbolic(alpha, t, k) for t in TIMEPOINTS])
+    # 4b. Create velocity profiles for the edge of the SDF
+    initial_velocities = np.array([initialize_velocity_exponential(alpha, t, k) if use_exponential else initialize_velocity_hyperbolic(alpha, t, k) for t in timepoints])
     inverted_initial_velocities_normalized = invert(initial_velocities / alpha)
     
-    # 3c. Create radius and width of the cut over time
-    radii = [0] * ITERATIONS
-    widths = [0] * ITERATIONS
+    # 4c. Create radius and width of the cut over time
+    radii = [0] * iterations
+    widths = [0] * iterations
 
-    for i in range(ITERATIONS):
+    for i in range(iterations):
         if i == 0:
             radii[i] = 0
             widths[i] = 0
             continue
 
         radii[i] = inverted_initial_velocities_normalized[i] * sdf_radius
-        widths[i] = widths[i - 1] + TIME_STEP * initial_velocities[i]
+        widths[i] = widths[i - 1] + time_step * initial_velocities[i]
 
-    # 4. Compute SDF and Velocity Fields
-    # 4a. Compute SDF fields
-    sdf_fields = compute_rounded_sdfs(WIDTH, HEIGHT, cola_cut.cut_origin, cola_cut.cut_destination, widths, radii, False, sdf_box, ITERATIONS)
+    # 5. Compute SDF and Velocity Fields
+    # 5a. Compute SDF fields
+    sdf_fields = compute_rounded_sdfs(WIDTH, HEIGHT, cola_cut.cut_origin, cola_cut.cut_destination, widths, radii, False, sdf_box, iterations)
     
-    # 4b. Create SDF Masks and Clipped SDF
+    # 5b. Create SDF Masks and Clipped SDF
     masked_sdf_fields = np.array([mask_on_condition(sdf < 0, 0, 1) for sdf in sdf_fields]) 
     clipped_rounded_sdf_fields = np.array([mask_on_condition(sdf < 0, 0, sdf) for sdf in sdf_fields])
 
-    # 4c. Compute raw velocity fields
-    velocity_fields = compute_velocity_fields(clipped_rounded_sdf_fields, prior_exp, TIMEPOINTS, initial_velocities, falloff_distance, ITERATIONS)
+    # 5c. Compute raw velocity fields
+    velocity_fields = compute_velocity_fields(clipped_rounded_sdf_fields, prior_exp, timepoints, initial_velocities, falloff_distance, iterations)
     
-    # 4d. Create masked velocity fields and zero out first timestep
+    # 5d. Create masked velocity fields and zero out first timestep
     masked_velocity_fields = np.array([vf * msdf for (vf, msdf) in zip(velocity_fields, masked_sdf_fields)])
     masked_velocity_fields[0, :, :] = np.where(clipped_rounded_sdf_fields[0, :, :] < 1, masked_velocity_fields[0, :, :], 0)
     
-    # 4e. Compute SDF gradients
+    # 5e. Compute SDF gradients
     sdf_gradients = directional_gradients(clipped_rounded_sdf_fields)
     sdf_dys = sdf_gradients[:, 0, :, :]
     sdf_dxs = sdf_gradients[:, 1, :, :]
 
-    # 5. Perform Backward Advection 
-    # 5a. Backward advection for the actomyosin cortex
+    # 6. Perform Backward Advection 
+    # 6a. Backward advection for the actomyosin cortex
     advected_cortex = advect_backward_sim(
         cortex.data, 
         sdf_dxs, 
         sdf_dys, 
         masked_velocity_fields, 
-        ITERATIONS, 
-        TIME_STEP
+        iterations, 
+        time_step
     )
 
-    # 5b. Backward advection for the myosin and actin layers separately
+    # 6b. Backward advection for the myosin and actin layers separately
     advected_myosin = advect_backward_sim(
         cortex.myosin_channel,
         sdf_dxs,
         sdf_dys,
         masked_velocity_fields,
-        ITERATIONS,
-        TIME_STEP
+        iterations, 
+        time_step
     )
 
     advected_actin = advect_backward_sim(
@@ -128,20 +128,20 @@ def gen_advected_cortex_worker(idx: int, out: str, max_offset: int, extent_major
         sdf_dxs,
         sdf_dys,
         masked_velocity_fields,
-        ITERATIONS,
-        TIME_STEP
+        iterations, 
+        time_step
     )
     
-    # 6. Save the advected fields and the velocity field
-    # 6a. Generate a UUID for this cell, just have a unique identifier
+    # 7. Save the advected fields and the velocity field
+    # 7a. Generate a UUID for this cell, just have a unique identifier
     cell_uuid = shortuuid.uuid()
 
-    # 6b. Save the advected cortex and its separate layers
+    # 7b. Save the advected cortex and its separate layers
     save_array(advected_cortex, Path(out) / f'cortex_{cell_uuid}_actomyosin.npy')
     save_array(advected_myosin, Path(out) / f'cortex_{cell_uuid}_myosin.npy')
     save_array(advected_actin, Path(out) / f'cortex_{cell_uuid}_actin.npy')
 
-    # 6c. Save the velocity field that was used to advect the cortex
+    # 7c. Save the velocity field that was used to advect the cortex
     save_array(masked_velocity_fields, Path(out) / f'cortex_{cell_uuid}_velocity_field.npy')
 
     # Return useful metadata
@@ -169,9 +169,9 @@ def gen_advected_cortex_worker(idx: int, out: str, max_offset: int, extent_major
         # Simulation metadata
         'width': WIDTH,
         'height': HEIGHT,
-        't_min': T_MIN,
-        't_max': T_MAX,
-        'step': TIME_STEP
+        't_min': t_min,
+        't_max': t_max,
+        'step': time_step
     }
 
 
@@ -180,6 +180,7 @@ def main():
     parser.add_argument("--n", type=int, required=True, help="Total number of cortices to generate")
     parser.add_argument("--workers", type=int, default=1, help="Number of workers for parallel processing")
     parser.add_argument("--out", type=str, default="./data", help="Output directory for generated data")
+    parser.add_argument("--step", type=float, default=0.5, help="Timestep to use in the advection simulation")
     parser.add_argument("--max_offset", type=int, default=100, help="Max offset from center for cell placement")
     parser.add_argument("--extent_long", type=int, default=450, help="Max extent for the major axis of the cell")
     parser.add_argument("--extent_short", type=int, default=300, help="Max extent for the minor axis of the cell")
@@ -188,7 +189,7 @@ def main():
     Path(args.out).mkdir(exist_ok=True)
 
     # Prepare tasks
-    tasks = [(i, args.out, args.max_offset, args.extent_long, args.extent_short) for i in range(args.n)]
+    tasks = [(i, args.out, args.step, args.max_offset, args.extent_long, args.extent_short) for i in range(args.n)]
 
     results = [0] * args.n
     with ProcessPoolExecutor(max_workers=args.workers) as executor:
